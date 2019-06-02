4f243f8239f239b564cb58e1d2c06ecf15dc7d55
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-06-02 15:32:27 -0300

    adding debug

--- app/bot_client.rb
+++ app/bot_client.rb
@@ -2,9 +2,11 @@ require 'telegram/bot'
 class BotClient
   def initialize(token = ENV['TELEGRAM_TOKEN'])
     @token = token
+    @logger = Logger.new(STDOUT)
   end
 
   def handle_message(message, bot)
+    @logger.debug "@#{message.from.username}: #{message.inspect}"
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Hola #{message.from.first_name}")
@@ -18,12 +20,13 @@ class BotClient
   end
 
   def start
+    @logger.info "token is #{@token}"
     run_client do |bot|
       bot.listen { |message| handle_message(message, bot) }
     end
   end
 
   def run_client(&block)
-    Telegram::Bot::Client.run(@token) { |bot| block.call bot }
+    Telegram::Bot::Client.run(@token, logger: @logger) { |bot| block.call bot }
   end
 end
