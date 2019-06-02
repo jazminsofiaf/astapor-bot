b87fffda02b37c0d940ef67bb592ea16de6e2ee0
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-06-02 12:25:09 -0300

    first test in bot

--- app/bot_client.rb
+++ app/bot_client.rb
@@ -1,2 +1,15 @@
+require 'telegram/bot'
 class BotClient
+  def initialize(token)
+    @token = token
+  end
+
+  def run
+    Telegram::Bot::Client.run(@token) do |bot|
+      bot.fetch_updates do |message|
+        bot.api.send_message(chat_id: message.chat.id,
+                             text: "Hola #{message.from.first_name}")
+      end
+    end
+  end
 end
--- spec/bot_client_spec.rb
+++ spec/bot_client_spec.rb
@@ -1,7 +1,47 @@
 require 'webmock/rspec'
+require_relative '../app/bot_client'
+
+def get_updates(token, text_message)
+  body = { "ok": true, "result": [{ "update_id": 115_760_237,
+                                    "message": { "message_id": 1,
+                                                 "from": { "id": 201_878_053,
+                                                           "is_bot": false,
+                                                           "first_name": 'Jazmin',
+                                                           "last_name": 'Ferreiro' },
+                                                 "chat": { "id": 201_878_053,
+                                                           "first_name": 'Jazmin',
+                                                           "last_name": 'Ferreiro',
+                                                           "type": 'private' },
+                                                 "date": 1_559_434_401,
+                                                 "text": text_message,
+                                                 "entities": [{ "offset": 0,
+                                                                "length": 6,
+                                                                "type": 'bot_command' }] } }] }
+
+  stub_request(:any, "https://api.telegram.org/bot#{token}/getUpdates")
+    .to_return(body: body.to_json, status: 200, headers: { 'Content-Length' => 3 })
+end
+
+def send_message(token, text_message)
+  stub_request(:post, "https://api.telegram.org/bot#{token}/sendMessage")
+    .with(
+      body: { 'chat_id' => '201878053', 'text' => text_message },
+      headers: {
+        'Accept' => '*/*',
+        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'User-Agent' => 'Faraday v0.15.4'
+      }
+    )
+    .to_return(status: 200, body: ''.to_json, headers: {})
+end
 
 describe 'BotClient' do
-  it 'should do something' do
-    true.should == true
+  it 'should get a /start message and respond with Hola ' do
+    token = 'fake_token'
+    get_updates(token, '/start')
+    send_message(token, 'Hola Jazmin')
+    app = BotClient.new(token)
+    app.run
   end
 end
